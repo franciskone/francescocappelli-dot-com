91b197ae1f961420f211f0da4ac3c317c8808ede
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2021-04-05 17:10:38 +0100

    add button stories

--- components/Button.stories.tsx
+++ components/Button.stories.tsx
@@ -5,27 +5,68 @@ import { Colors } from '../types';
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-// Here we export a variant of the default template passing props
-export const ButtonPrimary = Template.bind({});
-ButtonPrimary.args = {
+export const Interactive = Template.bind({});
+Interactive.args = {
   text: 'Click Me',
   onClick: () => alert('You clicked the button!'),
   color: Colors.primary,
 };
 
-// Here we export a variant of the default template passing props
-export const ButtonDisabled = Template.bind({});
-ButtonDisabled.args = {
-  ...ButtonPrimary.args,
+export const Primary = Template.bind({});
+Primary.args = {
+  ...Interactive.args,
+  color: Colors.primary,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  ...Interactive.args,
+  color: Colors.secondary,
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  ...Interactive.args,
+  color: Colors.success,
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  ...Interactive.args,
+  color: Colors.danger,
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  ...Interactive.args,
+  color: Colors.warning,
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  ...Interactive.args,
+  color: Colors.info,
+};
+
+export const Black = Template.bind({});
+Black.args = {
+  ...Interactive.args,
+  color: Colors.black,
+};
+
+export const White = Template.bind({});
+White.args = {
+  ...Interactive.args,
+  color: Colors.white,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Interactive.args,
   disabled: true,
 };
 
-// Here we export the default component that
-// will be used by Storybook to show it inside the sidebar
 export default {
   title: 'Button',
   component: Button,
-  argTypes: {
-    disabled: { control: 'boolean' },
-  },
 };
