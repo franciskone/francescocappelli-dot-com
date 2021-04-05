6fcdc7f222494d80a5107f5b725b6c21d8ca5411
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2021-04-05 11:22:22 +0100

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Ciaone</main>
+      <main>Ciaone</main>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 };
