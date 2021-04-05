382e113ce295e9509a84046417f9d14c2d9b7cb2
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2021-04-05 19:28:40 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,7 @@ import Head from 'next/head';
 import { Button } from '@windmill/react-ui';
 
 const Home = () => {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>Ciaone</main>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">
-          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
-        </div>
-      </div>
-
-      <Button size="larger">Larger</Button>
-      <Button size="large">Large</Button>
-      <Button>Regular</Button>
-      <Button size="small">Small</Button>
-    </div>
-  );
+  return <div>WIP</div>;
 };
 
 export default Home;
